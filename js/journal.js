370f0ab30e62a0064e1cacc12a1bd8df38c7505f
Maduabuchi Chinaechetam <lordcheta@YV-Cheta.local>
2021-02-24 19:03:20 +0100

    wip:pagination implementation

--- js/journal.js
+++ js/journal.js
@@ -1,4 +1,7 @@
 let test = document.getElementById('generalList'); // Gets the div inside which data will be rendered;
+let pagination = document.getElementById('pagination');
+let currentPage = 1; // Global variable keep track of the current page being rendered;
+let renderSize = 10; // The number of data to display
 
 
 function renderList(itemsToRender) { // Renders the amount of info giving to the UI
@@ -13,6 +16,24 @@ function renderList(itemsToRender) { // Renders the amount of info giving to the
     }
 };
 
+function renderPagination(dataSize, activePage, displaySize) {
+    let noOfPages = Math.ceil(dataSize / displaySize);
+    console.log(dataSize, displaySize, noOfPages);
+    let index = 0;
+    for (index; index < noOfPages; index++) {
+        let pageNumber = index + 1;
+        let template;
+        if(activePage == pageNumber) {
+            template = `<li class="page-item active"><a class="page-link" href="javascript:void(0);">${pageNumber}</a></li>`;
+        } else {
+            template = `<li class="page-item"><a class="page-link" href="javascript:void(0);">${pageNumber}</a></li>`;
+
+        }
+        pagination.insertAdjacentHTML('beforeend', template);
+        console.log(index);
+    }
+};
+
 // Fetch is used to load the static json file contianing relevasnt info
 fetch("/js/journals.json")
 .then(function (response) {
@@ -22,5 +43,6 @@ fetch("/js/journals.json")
     let dataArr; // Array to contain the json document for rendering
     dataArr = result.data;
     // console.log(dataArr);
+    renderPagination(dataArr.length, currentPage, renderSize);
     renderList(dataArr);
 });
