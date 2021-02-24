201d10ce3faa47c3f0a92cdec421bb5f3edeb5f6
Maduabuchi Chinaechetam <lordcheta@YV-Cheta.local>
2021-02-24 18:19:54 +0100

    wip:pagination implementation

--- js/journal.js
+++ js/journal.js
@@ -1,5 +1,26 @@
-let test = document.getElementById('test');
+let test = document.getElementById('generalList'); // Gets the div inside which data will be rendered;
 
-let template = `<p>Test test</p>`
 
-test.insertAdjacentHTML('afterbegin', template);
+function renderList(itemsToRender) { // Renders the amount of info giving to the UI
+    for (const item of itemsToRender) {
+        let template = 
+        `<div id="generalList">
+            <div class="list">
+                <p class="listText">${item.title}</p>
+            </div>
+        </div>`;
+        test.insertAdjacentHTML('afterbegin', template);
+    }
+};
+
+// Fetch is used to load the static json file contianing relevasnt info
+fetch("/js/journals.json")
+.then(function (response) {
+    return response.json();
+})
+.then(function(result) {
+    let dataArr; // Array to contain the json document for rendering
+    dataArr = result.data;
+    // console.log(dataArr);
+    renderList(dataArr);
+});
