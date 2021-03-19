eb65a298c6c76ecb511cfc9953c49c2b2bf14116
Chidera Olibie <jenniferolibie@gmail.com>
2021-03-19 17:29:18 +0100

    [DESKTOP] added about transition

--- js/helpers.js
+++ js/helpers.js
@@ -5,8 +5,19 @@ $(".about-content").on('click', function() {
 	$(".about-header h2").removeClass("about-active");
 	$(`#${className}-header`).addClass("about-active");
 	
+	// change pointer
+	$(".timeline div").removeClass("right");
+	$(this).parent().addClass("right");
 	// change content
 	$(".about-details p").removeClass("about-active");
 	$(`#${className}`).addClass("about-active");
 });
 
+// about page toggle
+// $(".content").on('click', function() {
+// 	const className = $(this).text().toLowerCase().trim();
+	
+// 	// change content
+// 	$(".about-details p").removeClass("about-active");
+// 	$(`#${className}`).addClass("about-active");
+// });
