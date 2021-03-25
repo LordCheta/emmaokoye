33b974fb47f978ff34d6376f96a8f810dab0453d
Chidera Olibie <jenniferolibie@gmail.com>
2021-03-25 02:23:46 +0100

    [MOBILE] test features done

--- js/helpers.js
+++ js/helpers.js
@@ -4,14 +4,27 @@ $(".about-content").on('click', function() {
 	// add class to about header
 	$(".about-header h2").removeClass("about-active");
 	$(`#${className}-header`).addClass("about-active");
-	
-	// change pointer
-	$(".timeline div").removeClass("right");
-	$(this).parent().addClass("right");
-	// change content
-	$(".about-details p").removeClass("about-active");
-	$(`#${className}`).addClass("about-active");
 });
+
+// $(".about-content").on('click', function() {
+// 	const className = $(this).text().toLowerCase().trim();
+// 	// add class to about header
+// 	$(".about-header h2").removeClass("about-active");
+// 	$(`#${className}-header`).addClass("about-active");
+	
+// 	// change pointer
+// 	$(".timeline div").removeClass("right");
+// 	$(this).parent().addClass("right");
+
+// 	// change content
+// 	$(".about-details p").removeClass("about-active");
+// 	$(`#${className}`).addClass("about-active");
+
+// 	// change icon
+// 	// $(".about-ul li").removeClass("about-li-active");
+// 	// console.log(className)
+// 	// $(`#${className}-li`).addClass("about-li-active");
+// });
 
 // about page toggle
 // $(".content").on('click', function() {
